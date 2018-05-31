9c72968214a2293d32abeebfc9efc01ce365f1eb
Adele Bendayan <adele.bendayan@gmail.com>
2018-05-31 18:40:48 +0300

    text display class

--- src/display/TextGridWorldDisplay.java
+++ src/display/TextGridWorldDisplay.java
@@ -5,6 +5,7 @@ import game.Directions;
 import grid.Grid;
 import mdp.Gridworld;
 import utils.Counter;
+import utils.Function;
 import utils.Pair;
 import utils.Tuple;
 
@@ -178,10 +179,10 @@ public class TextGridWorldDisplay implements GridWorldDisplay {
         ArrayList<ArrayList<String>> finalRows = new ArrayList<ArrayList<String>>();
         finalRows.add(colLabels);
         finalRows.addAll(newRows);
-        indent(finalRows, true, "-", "|", "center", true, "|", "|");
+        System.out.println(indent(finalRows, true, "-", "|", "center", true, "|", "|"));
     }
 
-    private void indent(ArrayList<ArrayList<String>> rows, boolean hasHeader, String headerChar, String delim,
+    private String indent(ArrayList<ArrayList<String>> rows, boolean hasHeader, String headerChar, String delim,
                         String justify, boolean seperateRows, String prefix, String postfix) {
         // break each logical row into one or more physical ones
         ArrayList<ArrayList<ArrayList<String>>> logicalRows = new ArrayList<>();
@@ -190,7 +191,7 @@ public class TextGridWorldDisplay implements GridWorldDisplay {
             arrayRows.addAll(rowWrapper(row));
         }
         // columns of physical rows
-        ArrayList<ArrayList<String>> columnLogical = new ArrayList<>();
+        ArrayList<ArrayList<String>> columns = new ArrayList<>();
         int maxLen = 0;
         for(ArrayList<ArrayList<String>> aRow : logicalRows) {
             if(aRow.size() > maxLen) {
@@ -198,10 +199,63 @@ public class TextGridWorldDisplay implements GridWorldDisplay {
             }
         }
         for(int i = 0; i < maxLen; i++) {
+            columns.add(new ArrayList<>());
             for(ArrayList<ArrayList<String>> aRow : logicalRows) {
-                columnLogical.get(i).add(aRow.get(0).get(i));
+                columns.get(i).add(aRow.get(0).get(i));
             }
         }
+        ArrayList<Integer> maxWidth = new ArrayList<>();
+        int totalMaxWidth = 0;
+        for(ArrayList<String> column: columns) {
+            int currentMax = 0;
+            for(String item : column) {
+                if(item.length() > currentMax) {
+                    currentMax = item.length();
+                }
+            }
+            maxWidth.add(currentMax);
+            totalMaxWidth += currentMax;
+        }
+        String rowSeparator = "";
+        for(int i = 0; i < (prefix.length() + postfix.length() + totalMaxWidth + (delim.length()*(maxWidth.size()-1))); i++) {
+            rowSeparator += headerChar;
+        }
+        String indentResult = "";
+        if(seperateRows) {
+            indentResult += rowSeparator;
+            indentResult += "\n";
+        }
+        int i;
+        for(ArrayList<ArrayList<String>> physicalRows : logicalRows) {
+            i = 0;
+            for(ArrayList<String> row : physicalRows) {
+                indentResult += prefix;
+                i+=1;
+                for(String item : row) {
+                    switch (justify) {
+                        case "center":
+                            indentResult += Function.centerString(maxWidth.get(i), item);
+                            break;
+                        case "left":
+                            indentResult += Function.padLeft(maxWidth.get(i), item);
+                            break;
+                        case "right":
+                            indentResult += Function.padRight(maxWidth.get(i), item);
+                            break;
+                    }
+                    if(i < row.size() - 1) {
+                        indentResult += delim;
+                    }
+                }
+                indentResult += postfix;
+            }
+            if(seperateRows || hasHeader) {
+                hasHeader = false;
+                indentResult += rowSeparator;
+                indentResult += "\n";
+            }
+        }
+        return indentResult;
     }
 
     private ArrayList<ArrayList<String>> rowWrapper(ArrayList<String> row) {
@@ -232,11 +286,221 @@ public class TextGridWorldDisplay implements GridWorldDisplay {
     }
 
     private void prettyPrintNullValues(Tuple currentState) {
+        Grid grid = this.gridworld.grid;
+        int maxLen = 11;
+        ArrayList<ArrayList<String>> newRows = new ArrayList<>();
+        for(int y = 0; y < grid.height; y++) {
+            ArrayList<String> newRow = new ArrayList<>();
+            for(int x = 0; x < grid.width; x++) {
+                Tuple state = new Tuple(x, y);
+                Directions action = null;
+                ArrayList<Directions> actions = this.gridworld.getPossibleActions(state);
+                if(actions.contains(Directions.EXIT)) {
+                    action = Directions.EXIT;
+                }
+                ArrayList<String> valString = new ArrayList<>();
+                if(grid.data[x][y].equals("S")) {
+                    valString.add("\n\nS\n\n");
+                    for(int i = 0; i < maxLen; i++) {
+                        valString.add(" ");
+                    }
+                }
+                else if(grid.data[x][y].equals("#")) {
+                    valString.add("\n#####\n#####\n#####\n");
+                    for(int i = 0; i < maxLen; i++) {
+                        valString.add(" ");
+                    }
+                }
+                else if(Function.isInteger(grid.data[x][y]) || Function.isFloat(grid.data[x][y])) {
+                    valString.add(border(grid.data[x][y]));
+                }
+                else {
+                    valString.add(border("  "));
+                }
+
+                if(currentState.equals(state)) {
+                    int l = valString.get(1).length();
+                    if(l == 0) {
+                        valString.set(1, "*");
+                    }
+                    else {
+                        StringBuilder temp = new StringBuilder("|");
+                        for(int i = 0; i < ((l-1)/2) -1; i++) {
+                            temp.append(" ");
+                        }
+                        temp.append("*");
+                        for(int i = 0; i < ((l)/2) -1; i++) {
+                            temp.append(" ");
+                        }
+                        temp.append("|");
+                        valString.set(1, temp.toString());
+                    }
+                }
+                StringBuilder temp = new StringBuilder();
+                for(int i = 0; i < maxLen/2; i++) {
+                    temp.append(" ");
+                }
+                switch (action) {
+                    case EAST:
+                        valString.set(2, "  " + valString.get(2) + " >");
+                        break;
+                    case WEST:
+                        valString.set(2, "< " + valString.get(2) + "  ");
+                        break;
+                    case NORTH:
+                        valString.set(0, temp.toString() + "^" + temp.toString());
+                        break;
+                    case SOUTH:
+                        valString.set(0, temp.toString() + "v" + temp.toString());
+                        break;
+                }
+                String newCell = "";
+                for(String val : valString) {
+                    newCell += val + "\n";
+                }
+                newRow.add(newCell);
+            }
+            newRows.add(newRow);
+        }
+        int numCols = grid.width;
+        for(int i = 0; i < newRows.size(); i++) {
+            newRows.get(i).add(0, "\n\n" + Integer.toString(i));
+        }
+        Collections.reverse(newRows);
+        ArrayList<String> colLabels = new ArrayList<>();
+        for(int colNum = 0; colNum < numCols; colNum++) {
+            colLabels.add(Integer.toString(colNum));
+        }
+        colLabels.add(0, " ");
+        ArrayList<ArrayList<String>> finalRows = new ArrayList<ArrayList<String>>();
+        finalRows.add(colLabels);
+        finalRows.addAll(newRows);
+        System.out.println(indent(finalRows, true, "-", "|", "center", true, "|", "|"));
 
     }
 
     private void prettyPrintQValues(Counter<Pair<Tuple, Directions>> qvalues, Tuple currentState, String message) {
-
+        Grid grid = this.gridworld.grid;
+        int maxLen = 11;
+        ArrayList<ArrayList<String>> newRows = new ArrayList<>();
+        for(int y = 0; y < grid.height; y++) {
+            ArrayList<String> newRow = new ArrayList<>();
+            for(int x = 0; x < grid.width; x++) {
+                Tuple state = new Tuple(x, y);
+                ArrayList<Directions> actions = this.gridworld.getPossibleActions(state);
+                if(actions.size() == 0) {
+                    actions = new ArrayList<>();
+                    actions.add(null);
+                }
+                Float bestQ = 0f;
+                for(Directions action : actions) {
+                    if(qvalues.count(new Pair<>(state, action)) > bestQ) {
+                        bestQ = qvalues.count(new Pair<>(state, action));
+                    }
+                }
+                ArrayList<Directions> bestActions = new ArrayList<>();
+                for(Directions action : actions) {
+                    if(qvalues.count(new Pair<>(state, action)) == bestQ) {
+                        bestActions.add(action);
+                    }
+                }
+                Map<Directions, String> qStrings = new HashMap<>();
+                for(Directions action : actions) {
+                    qStrings.put(action, String.valueOf(qvalues.count(new Pair<>(state, action))));
+                }
+                String northString = " ";
+                if(qStrings.containsKey(Directions.NORTH)) {
+                    northString = "north";
+                }
+                String southString = " ";
+                if(qStrings.containsKey(Directions.SOUTH)) {
+                    southString = "south";
+                }
+                String eastString = " ";
+                if(qStrings.containsKey(Directions.EAST)) {
+                    eastString = "east";
+                }
+                String westString = " ";
+                if(qStrings.containsKey(Directions.WEST)) {
+                    westString = "west";
+                }
+                String exitString = " ";
+                if(qStrings.containsKey(Directions.EXIT)) {
+                    exitString = "exit";
+                }
+                int eastLen = eastString.length();
+                int westLen = westString.length();
+                if(eastLen < westLen) {
+                    String temp = "";
+                    for(int i = 0; i < (westLen - eastLen); i++) {
+                        temp += " ";
+                    }
+                    eastString = temp + eastString;
+                }
+                if(westLen < eastLen) {
+                    String temp = "";
+                    for(int i = 0; i < (eastLen - westLen); i++) {
+                        temp += " ";
+                    }
+                    westString += temp;
+                }
+                if(bestActions.contains(Directions.NORTH)) {
+                    northString = "/" + northString + "\\";
+                }
+                if(bestActions.contains(Directions.SOUTH)) {
+                    southString = "\\" + southString + "/";
+                }
+                if(bestActions.contains(Directions.EAST)) {
+                    eastString+= ">";
+                }
+                else {
+                    eastString += " ";
+                }
+                if(bestActions.contains(Directions.WEST)) {
+                    westString = "<" + westString;
+                }
+                else {
+                    westString = " " + westString;
+                }
+                if(bestActions.contains(Directions.EXIT)) {
+                    exitString = "[ " + exitString + " ]";
+                }
+                String ewString = westString + "     " + eastString;
+                if(state.equals(currentState)) {
+                    ewString = westString + "  *  " + eastString;
+                }
+                if(state.equals(this.gridworld.getStartState())) {
+                    ewString = westString + "  S  " + eastString;
+                }
+                if(state.equals(currentState) && state.equals(this.gridworld.getStartState())) {
+                    ewString = westString + " S:* " + eastString;
+                }
+                String text = northString + "\n" + "\n"+exitString + "\n" + ewString + "\n";
+                for(int i = 0; i < maxLen; i++) {
+                    text += " ";
+                }
+                text += "\n\n" + southString;
+                if(grid.data[x][y].equals("#")) {
+                    text = "\n\n#####\n#####\n#####\n";
+                }
+                newRow.add(text);
+            }
+            newRows.add(newRow);
+        }
+        int numCols = grid.width;
+        for(int i = 0; i < newRows.size(); i++) {
+            newRows.get(i).add(0, "\n\n\n" + Integer.toString(i));
+        }
+        Collections.reverse(newRows);
+        ArrayList<String> colLabels = new ArrayList<>();
+        for(int colNum = 0; colNum < numCols; colNum++) {
+            colLabels.add(Integer.toString(colNum));
+        }
+        colLabels.add(0, " ");
+        ArrayList<ArrayList<String>> finalRows = new ArrayList<ArrayList<String>>();
+        finalRows.add(colLabels);
+        finalRows.addAll(newRows);
+        System.out.println(indent(finalRows, true, "-", "|", "center", true, "|", "|"));
     }
 
     private String border(String value) {
--- src/utils/Function.java
+++ src/utils/Function.java
@@ -17,4 +17,16 @@ public class Function {
     public static boolean isFloat(String s) {
         return s.matches("\\d*\\.?\\d*");
     }
+
+    public static String centerString (int width, String s) {
+        return String.format("%-" + width  + "s", String.format("%" + (s.length() + (width - s.length()) / 2) + "s", s));
+    }
+
+    public static String padRight(int n, String s) {
+        return String.format("%1$-" + n + "s", s);
+    }
+
+    public static String padLeft(int n, String s) {
+        return String.format("%1$" + n + "s", s);
+    }
 }
