f54f4d23c9c957b57c2e397ec076429c6ff16ed1
Adele Bendayan <adele.bendayan@gmail.com>
2018-06-20 13:02:58 +0300

    override equal for pair

--- src/utils/Pair.java
+++ src/utils/Pair.java
@@ -1,5 +1,7 @@
 package utils;
 
+import java.util.Objects;
+
 public class Pair<L,R> {
     private L l;
     private R r;
@@ -11,4 +13,14 @@ public class Pair<L,R> {
     public R getR(){ return r; }
     public void setL(L l){ this.l = l; }
     public void setR(R r){ this.r = r; }
+
+    @Override
+    public boolean equals(Object o) {
+        return o instanceof Pair && this.l.equals(((Pair) o).getL()) && this.r.equals(((Pair) o).getR());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(l, r);
+    }
 }
